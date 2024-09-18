6a2ef54dec09c6d2fe040b8276bbaaeb97f210f0
ThiagoDalicio <thiagodcfreire@gmail.com>
2024-09-18 10:15:32 -0300

    Delete operadoresLogicos.py

--- operadoresLogicos.py
+++
@@ -1,8 +0,0 @@
-senha = True
-dinheiro = True
-# no Conectivo AND, só executará o if se as condições forem VERDADEIRAS
-
-if senha and dinheiro:
-    print("Você está Feliz! Pode sacar!")
-else:
-    print("Acesso Negado")
