d8c22e2b9f222e3c941c321d78647309b0f769a3
ThiagoDalicio <thiagodcfreire@gmail.com>
2024-09-18 10:15:23 -0300

    Delete operadoresLogicos2.py

--- operadoresLogicos2.py
+++
@@ -1,8 +0,0 @@
-cartao = False
-dinheiro = False
-# no Conectivo OR, só não executa se TODAS as condições forem FALSAS 
-
-if cartao or dinheiro:
-    print("Compra Liberada")
-else:
-    print("Não Posso Comprar!")
